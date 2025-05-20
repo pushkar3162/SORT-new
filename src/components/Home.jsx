5fd7d51cf050047ea9f0ff8ee4272b5a694fc5f8
Renuka Patil <122209822+RenukaPatil3122@users.noreply.github.com>
2025-05-20 16:00:01 +0530

    Create Home.jsx

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="p-4" >
+      <h1 className="text-2xl font-bold text-gray-800">
+        Welcome to the Dashboard
+      </h1>
+      
+    </div>
+  );
+};
+
+export default Home;
